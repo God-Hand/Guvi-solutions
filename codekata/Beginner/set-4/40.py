a4eefb143e27c260272331cc2948417f9d0713a5
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-14 00:27:25 +0530

    Update codekata/Beginner/set-4/40.py

--- codekata/Beginner/set-4/40.py
+++ codekata/Beginner/set-4/40.py
@@ -1,3 +1,14 @@
 n = int(input())
 if n==1:
     print(1)
+elif n==2:
+    print(1,1)
+else:
+    a = 1
+    b = 1
+    print(1,1,end=' ')
+    for i in range(2,n):
+        c = a+b
+        a = b
+        b = c
+        print(c,end=' ')
