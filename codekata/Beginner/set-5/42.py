147c0208f5a65df798f52c4850919cf725da9e23
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-14 00:46:25 +0530

    Update codekata/Beginner/set-5/42.py

--- codekata/Beginner/set-5/42.py
+++ codekata/Beginner/set-5/42.py
@@ -1,3 +1,17 @@
-s, k = input().split()
-for _ in range(int(k)):
-    print(s)
+# trying to make it as c comparing
+s1, s2 = input().split()
+l1 = len(s1)
+l2 = len(s2)
+equal = True
+for i in range(len(max(l1,l2))):
+    if s1[i]>s2[i]:
+        print(s1)
+        equal = False
+        break
+    elif s1[i]<s2[i]:
+        print(s2)
+        equal = False
+        break
+
+if equal:
+    print(s1)
