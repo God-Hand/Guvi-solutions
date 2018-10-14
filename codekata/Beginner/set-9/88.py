4e77f86f51dd56cdcba86a11ca952022a6d3e5d2
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-10-14 16:57:04 +0530

    Update codekata/Beginner/set-9/88.py

--- codekata/Beginner/set-9/88.py
+++ codekata/Beginner/set-9/88.py
@@ -1,5 +1,5 @@
 a,b = map(int,input().split())
-for i in range(min(a,b),0,-1):
-    if not(a%i) and not(b%i):
+for i in range(max(a,b),(a*b)+1):
+    if not(i%a) and not(i%b):
         print(i)
         break
