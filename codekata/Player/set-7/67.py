ba3c33d3d32bc5d6015b11656434a54bf41b41ab
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-04 23:54:46 +0530

    Update codekata/Player/set-7/67.py

--- codekata/Player/set-7/67.py
+++ codekata/Player/set-7/67.py
@@ -1,6 +1,6 @@
-def fac(n):
+def factorial(n):
     if n==1 or n==0:
         return 1
-    return n*fac(n-1)
+    return n*factorial(n-1)
 
-print(fac(int(input())))
+print(factorial(int(input())))
