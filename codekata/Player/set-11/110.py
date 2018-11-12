414ef733ff5286e67c403e1459a6f238a7450441
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-11-13 00:34:24 +0530

    Update codekata/Player/set-11/110.py

--- codekata/Player/set-11/110.py
+++ codekata/Player/set-11/110.py
@@ -1,4 +1,4 @@
 n = int(input())
-l = sorted(list(map(int,input().split())),reverse=True)
-ml = l[:n//2]
-print(sum(ml)*(n-1)//(n//2))
+l = list(map(int,input().split()))
+for i in range(n):
+    print(sum(l[:i+1])+sum(l[i:]), end=' ')
