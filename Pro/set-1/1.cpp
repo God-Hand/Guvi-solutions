4f2d696c47f1bb215c5ce734fee5f38ad22236df
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-12-28 22:15:41 +0530

    set-1 completed

--- Pro/set-1/1.cpp
+++ Pro/set-1/1.cpp
@@ -2,5 +2,32 @@
 using namespace std;
 
 int main(){
+    int i, n, min_length=INT_MAX;
+    cin>>n;
+    vector<string> vec(n);
+    string min_string;
+    for(int i=0; i<n; i++){
+        cin>>vec[i];
+        if(min_length > vec[i].length()){
+            min_length = vec[i].length();
+            min_string = vec[i];
+        }
+    }
+
+    bool stop = false;
+    for(i=0; i<min_length; i++){
+        for(int j=0; j<n; j++){
+            if(vec[j][i] != min_string[i]){
+                stop = true;
+                break;
+            }
+        }
+        if(stop)
+            break;
+    }
+    if(!stop)
+        cout<<min_string;
+    else
+        cout<<min_string.substr(0,i-1);
     return 0;
 }
