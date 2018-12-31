62fc7cdb5e57736e9f0c5e9c9c22a518f0f3b350
majestic_gohan <31720563+God-Hand@users.noreply.github.com>
2018-12-31 07:22:03 +0530

    set-3 completed

---
+++ Pro/set-3/22.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxSumWithoutNeighbours(vector<int> vec){
+    int include = vec[0];
+    int exclude = 0;
+    int temp_exclude = 0;
+    for(int i=1; i<vec.size(); i++){
+        temp_exclude = max(include , exclude);
+        include = exclude + vec[i];
+        exclude = temp_exclude;
+    }
+    return max(include, exclude);
+}
+
+    int n;
+    cin>>n;
+    vector<int> vec(n);
+    for(int i=0; i<n; i++)
+        cin>>vec[i];
+    cout<<maxSumWithoutNeighbours(vec);
+    return 0;
+}
